7d9d1b67ea766e1312f2b05228ed56300b8575b0
MAGILLUSIONAL <fantasyoui@gmail.com>
2013-05-01 19:28:17 +0800

    修改项目名

--- hook.class.php
+++ hook.class.php
@@ -10,22 +10,22 @@ if (!defined('IN_DISCUZ')) {
 	exit('Access Denied');
 }
 
-class plugin_dps_taobao_base {
+class plugin_dps_xtao_base {
 
 	function init() {
 		global $_G;
 		//include_once template('qqconnect:module');
-		if(!$_G['cache']['plugin']['dps_taobao']['allow'] || $_G['setting']['bbclosed']) {
+		if(!$_G['cache']['plugin']['dps_xtao']['allow'] || $_G['setting']['bbclosed']) {
 			return;
 		}
 		$this->allow = true;
 	}
 }
 
-class plugin_dps_taobao extends plugin_dps_taobao_base {
+class plugin_dps_xtao extends plugin_dps_xtao_base {
 	var $allow = false;
 
-	function plugin_dps_taobao() {
+	function plugin_dps_xtao() {
 		$this->init();
 	}
 	function global_login_extra() {
@@ -33,7 +33,7 @@ class plugin_dps_taobao extends plugin_dps_taobao_base {
 		if(!$this->allow || $_G['inshowmessage']) {
 			return;
 		}
-		include template('dps_taobao:_tpl_'.__FUNCTION__);
+		include template('dps_xtao:_tpl_'.__FUNCTION__);
 		return $return;
 	}
 }
