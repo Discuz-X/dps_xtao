241551c5814c2fc59883411421bd5bbc68918d50
MAGILLUSIONAL <fantasyoui@gmail.com>
2013-05-08 17:22:41 +0800

    统一资源配置

--- index.inc.php
+++ index.inc.php
@@ -5,6 +5,6 @@ if(!defined('IN_DISCUZ')) {
 
 //跳转到其它页
 //header('Location: plugin.php?id=abis_mall:index_classall');
-header('Location: plugin.php?id=abis_shops:tb_goods_search');
+header('Location: plugin.php?id=dps_xtao:tb_goods_search');
 
 ?>
