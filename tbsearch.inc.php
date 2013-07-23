12bbb0a7efa759c9d65267eee4fa3b0ee45978ff
SprayHank <fantasyoui@gmail.com>
2013-07-23 23:26:20 +0800

    页面跳转与变量传递

--- tbsearch.inc.php
+++ tbsearch.inc.php
@@ -1,4 +1,4 @@
 <?php defined('IN_DISCUZ') || die('Access Denied');
 include 'lib/functions.php';
-$keyword = $_GET['keyword'];
+$keyword = urlencode($_POST['keyword']);
 header('Location: plugin.php?id=dps_xtao:taobaoindex&sort=commissionNum_desc&keyword='.$keyword);
