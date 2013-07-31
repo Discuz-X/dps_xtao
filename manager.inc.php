5da0689fa37846b79d3f765128ead15880f32e8c
SprayHank <fantasyoui@gmail.com>
2013-07-31 21:18:53 +0800

    整理代码

--- manager.inc.php
+++ manager.inc.php
@@ -1,33 +1,26 @@
 <?php (!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) && exit('Access Denied');
-
 /**
  *      [Discuz!] (C)2001-2099 Comsenz Inc.
  *      This is NOT a freeware, use is subject to license terms
  *
  *      $Id: @lvenle@gmail.com
  */
-
-
 loadcache('plugin');
 loadcache('forums');
 $type = 0;
 global $_G;
-
 $vars   = $_G['cache']['plugin']['tkb'];
 $forums = $_G['cache']['forums'];
 $groups = (array)unserialize($vars['forums']);
 //var_dump($forums);// $groups
 $forumscat = array();
-
 foreach($groups as $fid) {
 	foreach($forums as $forum) {
 		if($forum['fid'] == $fid) {
 			$forumscat[] = $forum;
 		}
-
 	}
 }
-
 $pageNo = isset($_GET['page']) ? $_GET['page'] : 1;
 $type   = 0;
 if(!empty($_GET['fid'])) {
@@ -43,13 +36,9 @@ if($fid > 0) {
 	$items_service->items_dao = C::t('#tkb#tkb_items');
 	$offset                   = ($pageNo - 1) * 20;
 	$items                    = $items_service->get_items_by_type($fid, 0, $offset, 20);
-
-	$totalcount = count($items);
+	$totalcount               = count($items);
 }
-
-
 //echo $return;
-
 $subPage_link = 'admin.php?a=a';
 unset($_GET['fields']);
 foreach($_GET as $key => $value) {
@@ -57,19 +46,12 @@ foreach($_GET as $key => $value) {
 }
 //生成提交后用于返回的链接
 $back_url = $subPage_link.'&page='.$pageNo;
-
 include template('tkb:manager');
-
-
 if($totalcount == 0) {
-
 	echo '没有搜索结果';
-
 } else if($totalcount == 20) {
 	$totalcount = $items_service->get_items_count_by_type($fid, $type);
 	include dirname(__FILE__).'/lib/pages.class.php';
-
 	new SubPages(20, $totalcount, $pageNo, 10, $subPage_link, 2);
 }
-
 ?>
